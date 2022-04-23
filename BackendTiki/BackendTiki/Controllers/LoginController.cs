bc6f015432bc0099e3d7b9071e85cd968cdae774
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-04-23 15:51:41 +0700

    login by phone number

---
+++ BackendTiki/BackendTiki/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using BackendTiki.Access;
+using BackendTiki.Dto;
+using BackendTiki.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendTiki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly LoginService _service;
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration, Context context)
+        {
+            _configuration = configuration;
+            _service = new LoginService(configuration, context);
+        }
+        [Route("login")]
+        [HttpPost]
+        public JsonResult LoginByPhoneNumber(LoginDTO account)
+        {
+            string result = _service.LoginByPhoneNumber(account);
+
+            return String.IsNullOrEmpty(result) ?
+                new JsonResult(new { success = "faild",message="Login failues" }) : 
+                new JsonResult(new{ token= result });
+        }
+    }
+}
