ad78abf959b652e5471af98905c7a4f53a0c8ffd
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-05-09 21:35:08 +0700

    front-end

---
+++ BackendTiki/BackendTiki/BackendTiki/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using BackendTiki.Access;
+using BackendTiki.Models;
+
+namespace BackendTiki.Services
+{
+    public class UserService
+    {
+        private readonly Context _context;
+        private readonly IConfiguration _configuration;
+        public UserService(IConfiguration configuration, Context context)
+        {
+            _configuration = configuration;
+            _context = context;
+
+        }
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetById(string id)
+        {
+            return _context.Users.SingleOrDefault(e => e.UserId == id);
+        }
+
+        public User CreateUser(User user)
+        {
+            User _user= _context.Users.SingleOrDefault(e => e.PhoneNumber == user.PhoneNumber);
+            if ( _user == null)
+            {
+                Guid g = Guid.NewGuid();
+                user.UserId = g.ToString();
+                _context.Users.Add(user);
+                if (_context.SaveChanges()>0)
+                    return user;
+                return null;
+            }
+            return null;
+        }
+
+    }
+}
