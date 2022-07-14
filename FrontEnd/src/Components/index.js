1a30b4e0adadcb6e2e2677440d185f40ea1b7649
quochuy2405 <huy01682312920@gmail.com>
2022-07-14 11:57:26 +0700

    crawl data

--- FrontEnd/src/Components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header'
-export { default as Banner } from './Banner'
-export { default as CategoryItems } from './CategoryItems'
-export { default as ListProduct } from './ListProduct'
-export { default as Footer } from './Footer'
